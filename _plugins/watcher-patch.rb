23caf02d670d9c85efabf277f2eb4a9833d42cd0
zahraasalsabila <zhrslsa.2618181@gmail.com>
2025-06-26 18:41:23 +0800

    Fix watcher-patch plugin syntax error

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -1,9 +1,9 @@
 require 'jekyll-watch'
 
-module jekyll
+module Jekyll
     module Watcher
         extend self
-        alias_method :original_listen_ignore_paths, :original_listen_ignore_paths
+        alias_method :original_listen_ignore_paths, :listen_ignore_paths
 
         def listen_ignore_paths(option)
             original_listen_ignore_paths(option) + [%r!.*\.TMP!i]
